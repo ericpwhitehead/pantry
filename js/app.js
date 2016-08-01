efbc2949a3ac35ec188b2c4078bc2a5e128dde3f
ericpwhitehead <ericpwhitehead@gmail.com>
2016-07-31 23:13:48 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,30 +1,25 @@
 
 
 $(document).ready(function(){
-$("button#add").click(function(){
 
-	if ($("input#item").val()){
-	var itemToAdd = $("input#item").val();
-	$("ul.toget").append("<li><i id='uncheck' class='fa fa-square-o'></i> "+itemToAdd+" <i id='trash' class='fa fa-trash-o'></i></li>");
-	$("input#item").val("").focus();
-	}
+	$("button#add").on("click", function() {
 
-	$(".toget li").click(function() {
-	$(this).toggleClass("clicked");
+		if ($("input#item").val()) {
+		var itemToAdd = $("input#item").val();
+		$("ul.toget").append("<li class='added'><i id='uncheck' class='fa fa-square-o'></i><span> "+itemToAdd+" </span><i id='trash' class='fa fa-trash-o'></i></li>");
+		
+		$("#trash").click(function() {
+			$(this).parent("li").remove();
+		});
+
+		};
+		$("input#item").val("").focus();
+ 	});
+
+
+	$('ul').delegate('li','click',function() {
+		$(this).toggleClass('clicked');
 	});
-
-	$("#trash").click(function() {
-	$(".toget li").remove();
-
-	})
-
-	
-	
-
-
-	
-
- });
 
 
 });
