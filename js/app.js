642810a9a729a318e155deccd5143e62bab6c075
ericpwhitehead <ericpwhitehead@gmail.com>
2016-07-31 23:20:37 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,30 +1,35 @@
 
 
 $(document).ready(function(){
-$("button#add").click(function(){
+	$('input#item').on('keydown', function(e) {
+	    if (e.which == 13) {
+	       $("button#add").click();
+	    }
+	  });
+	$("button#add").on("click", function() {
 
-	if ($("input#item").val()){
-	var itemToAdd = $("input#item").val();
-	$("ul.toget").append("<li><i id='uncheck' class='fa fa-square-o'></i> "+itemToAdd+" <i id='trash' class='fa fa-trash-o'></i></li>");
-	$("input#item").val("").focus();
-	}
+		if ($("input#item").val()) {
+		var itemToAdd = $("input#item").val();
+		$("ul.toget").append("<li class='added'><i id='uncheck' class='fa fa-square-o'></i><span> "+itemToAdd+" </span><i id='trash' class='fa fa-trash-o'></i></li>");
+		};
 
-	$(".toget li").click(function() {
-	$(this).toggleClass("clicked");
+		$("input#item").val("").focus();
+ 	});
+
+
+	$('ul.toget').delegate('li','click',function() {
+		$(this).toggleClass('clicked');
+		if ($(this).children('i#uncheck').hasClass('fa-square-o')) {
+			$(this).children('i#uncheck').removeClass('fa-square-o');
+			$(this).children('i#uncheck').addClass('fa-check-square-o');
+		} else if ($(this).children('i#uncheck').hasClass('fa-check-square-o')) {
+			$(this).children('i#uncheck').addClass('fa-square-o');
+			$(this).children('i#uncheck').removeClass('fa-check-square-o');
+		};
 	});
-
-	$("#trash").click(function() {
-	$(".toget li").remove();
-
-	})
-
-	
-	
-
-
-	
-
- });
+	$('ul.toget').delegate('li i#trash','click',function() {
+		$(this).parent("li").remove();
+	});
 
 
 });
